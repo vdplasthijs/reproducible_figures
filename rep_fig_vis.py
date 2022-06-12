7488745c3cfd3507d34eeb9b07baa6c3026c8db3
Thijs <vdplasthijs@gmail.com>
2022-06-12 12:00:02 +0100

    finish exampl fig

--- rep_fig_vis.py
+++ rep_fig_vis.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
+import copy
 
 ##############################
 ###   GENERAL PLOTTING FUNCTIONS
 ##############################
 
 def despine(ax):
+    '''Remove top and right spines'''
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
 def naked(ax):
+    '''Remove all spines, ticks and labels'''
     for ax_name in ['top', 'bottom', 'right', 'left']:
         ax.spines[ax_name].set_visible(False)
     ax.set_xticks([])
@@ -19,9 +22,10 @@ def naked(ax):
     ax.set_ylabel('')
 
 def set_fontsize(font_size=12):
+    '''Set font size for all text elements'''
     plt.rcParams['font.size'] = font_size
     plt.rcParams['axes.autolimit_mode'] = 'data' # default: 'data'
-    params = {'legend.fontsize': font_size,
+    params = {'legend.fontsize': font_size,  # set for all kinds of text elements
             'axes.labelsize': font_size,
             'axes.titlesize': font_size,
             'xtick.labelsize': font_size,
@@ -30,6 +34,7 @@ def set_fontsize(font_size=12):
     print(f'Font size is set to {font_size}')
 
 def equal_xy_lims(ax, start_zero=False):
+    '''Set xlim equal to ylim (by their max)'''
     xlims = ax.get_xlim()
     ylims = ax.get_ylim()
     max_outer_lim = np.maximum(xlims[1], ylims[1])
@@ -43,7 +48,7 @@ def equal_xy_lims(ax, start_zero=False):
         ax.set_ylim([min_inner_lim, max_outer_lim])
 
 def equal_lims_two_axs(ax1, ax2):
-
+    '''Set xlim equal to ylim across two axes'''
     xlim_1 = ax1.get_xlim()
     xlim_2 = ax2.get_xlim()
     ylim_1 = ax1.get_ylim()
@@ -59,13 +64,42 @@ def equal_lims_two_axs(ax1, ax2):
     ax1.set_ylim([new_y_min, new_y_max])
     ax2.set_ylim([new_y_min, new_y_max])
 
-def remove_xticklabels(ax):  # remove labels but keep ticks
+def equal_lims_n_axs(ax_list):
+    '''Set ax lims equal across list of ax, but xlim and ylim are seperate here'''
+    for i_ax, ax in enumerate(ax_list):
+        x_min, x_max = ax.get_xlim()
+        y_min, y_max = ax.get_ylim()
+
+        if i_ax == 0:
+            x_min_min = copy.deepcopy(x_min)
+            x_max_max = copy.deepcopy(x_max)
+            y_min_min = copy.deepcopy(y_min)
+            y_max_max = copy.deepcopy(y_max)
+
+        else:
+            if x_min < x_min_min:
+                x_min_min = copy.deepcopy(x_min)           
+            if x_max > x_max_max:
+                x_max_max = copy.deepcopy(x_max)
+            if y_min < y_min_min:
+                y_min_min = copy.deepcopy(y_min)
+            if y_max > y_max_max:
+                y_max_max = copy.deepcopy(y_max)
+
+    for ax in ax_list:
+        ax.set_xlim([x_min_min, x_max_max])
+        ax.set_ylim([y_min_min, y_max_max])
+
+def remove_xticklabels(ax):  
+    '''remove x ticklabels but keep ticks'''
     ax.set_xticklabels(['' for x in ax.get_xticklabels()])
 
-def remove_yticklabels(ax):  # remove labels but keep ticks
+def remove_yticklabels(ax):  
+    '''remove y ticklabels but keep ticks'''
     ax.set_yticklabels(['' for x in ax.get_yticklabels()])
 
-def remove_both_ticklabels(ax):  # remove labels but keep ticks
+def remove_both_ticklabels(ax): 
+    '''both x and y'''
     remove_xticklabels(ax)
     remove_yticklabels(ax)
 
@@ -99,3 +133,25 @@ def plot_normal_distr(ax=None, n_tp=500, mean_distr=0, std_distr=1, alpha=1, col
     ax.plot(t_array, norm_array, linewidth=3, alpha=alpha, c=colour)
     ax.set_xlabel('Some variable')
     ax.set_ylabel('PDF')
+
+def plot_brown_proc(ax_trace=None, ax_hist=None, var=1, n_steps=500, 
+                    plot_ylabel=True, colour='k'):
+    '''Sample brownian motion & plot trace and histogram'''
+    gauss_samples = np.random.randn(n_steps) * np.sqrt(var)
+    brown_motion = np.cumsum(gauss_samples)
+    time_array = np.arange(n_steps)
+
+    if ax_trace is None or ax_hist is None:
+        fig, ax = plt.subplots(1, 2)
+        ax_trace, ax_hist = ax
+
+    ax_trace.plot(time_array, brown_motion, linewidth=2, c=colour)
+    ax_trace.set_xlabel('Iteration')
+    if plot_ylabel:
+        ax_trace.set_ylabel('Activity')
+    
+    ax_hist.hist(brown_motion, bins=np.linspace(-100, 100, 30),
+                 facecolor=colour, edgecolor='k', linewidth=1)
+    ax_hist.set_xlabel('Activity')
+    if plot_ylabel:
+        ax_hist.set_ylabel('Frequency')
