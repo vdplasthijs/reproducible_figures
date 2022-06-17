9283b2820dbd2294067a009554153cefcd4c8f06
prajayshah <prajay.shah@mail.utoronto.ca>
2022-06-17 09:59:40 -0400

    changed grid bounding order, needed for implementation of single axes grid

--- rep_fig_vis.py
+++ rep_fig_vis.py
@@ -9,6 +9,8 @@ import copy
 ##############################
 ###   GENERAL PLOTTING FUNCTIONS
 ##############################
+from matplotlib.transforms import Bbox
+
 
 def despine(ax):
     '''Remove top and right spines'''
@@ -237,27 +239,32 @@ def make_fig_layout(layout: dict = None, **kwargs):
 
         gs_ = fig.add_gridspec(ncols=_grid['panel_shape'][0], nrows=_grid['panel_shape'][1],
                                left=_grid['bound'][0],
-                               top=_grid['bound'][1],
+                               bottom=_grid['bound'][1],
                                right=_grid['bound'][2],
-                               bottom=_grid['bound'][3],
+                               top=_grid['bound'][3],
                                wspace=wspace, hspace=hspace
                                )  # leave a bit of space between grids (eg left here and right in grid above)
 
         n_axs: int = _grid['panel_shape'][0] * _grid['panel_shape'][1]
         # _axes = {}
         if _grid['panel_shape'][0] > 1 and _grid['panel_shape'][1] > 1:
-            _axes = np.empty(shape = (range(_grid['panel_shape'][0]), range(_grid['panel_shape'][1])), dtype=object)
+            _axes = np.empty(shape=(range(_grid['panel_shape'][0]), range(_grid['panel_shape'][1])), dtype=object)
             for col in range(_grid['panel_shape'][0]):
                 for row in range(_grid['panel_shape'][1]):
                     _axes[col, row] = fig.add_subplot(gs_[col, row])  # create ax by indexing grid object
 
         elif _grid['panel_shape'][0] > 1 or _grid['panel_shape'][1] > 1:
-            _axes = np.empty(shape = (n_axs), dtype=object)
+            _axes = np.empty(shape=(n_axs), dtype=object)
             for i in range(n_axs):
                 _axes[i] = fig.add_subplot(gs_[i])  # create ax by indexing grid object
 
-        elif _grid['panel_shape'][0] == 1 or _grid['panel_shape'][1] == 1:
-            raise NotImplementedError('action not implemented yet.')
+        elif _grid['panel_shape'][0] == 1 and _grid['panel_shape'][1] == 1:
+            # positions = gs_.get_grid_positions(fig)
+            bbox = Bbox.from_extents(_grid['bound'][0], _grid['bound'][1], _grid['bound'][2], _grid['bound'][3])
+            _axes = np.empty(shape=(n_axs), dtype=object)
+            ax = fig.add_subplot()
+            ax.set_position(pos=bbox)
+            _axes[0] = ax
         else:
             raise NotImplementedError('action not implemented yet.')
 
@@ -265,6 +272,7 @@ def make_fig_layout(layout: dict = None, **kwargs):
         grids[name] = gs_
 
     return fig, axes, grids
+
 
 def make_random_scatter(ax, title):
     ax.scatter(np.random.randn(100), np.random.randn(100), s=50, c='forestgreen')
@@ -276,19 +284,24 @@ def make_random_scatter(ax, title):
 def show_test_figure_layout(fig, axes):
     for grid, panels in axes.items():
         # print(panels)
+        if len(panels) == 1:
+            make_random_scatter(ax=panels[0], title=f"{grid} - ax: {0}")
         for i, ax in enumerate(panels):
-            make_random_scatter(ax=ax, title = f"{grid} - ax: {i}")
+            make_random_scatter(ax=ax, title=f"{grid} - ax: {i}")
     fig.show()
 
 
 def add_label_grid(grid, s, fig, ax, **kwargs):
     """Add text annotation in relation to a specified gridspec object."""
     fs = 15 if 'fontsize' not in kwargs else kwargs['fontsize']
+    y_adjust = 0.02 if 'y_adjust' not in kwargs else kwargs['y_adjust']
+    x_adjust = 0.04 if 'x_adjust' not in kwargs else kwargs['x_adjust']
+
     gs_ = grid
     pos = gs_.get_grid_positions(fig)
     top = np.max(pos[1])
     left = np.max(pos[2])
-    xy = (left - 0.04, top + 0.02)
+    xy = (left - x_adjust, top + y_adjust)
     ax.annotate(s=s, xy=xy, xycoords='figure fraction', fontsize=fs, weight='bold')
 
 
@@ -306,9 +319,5 @@ def add_label_axes(s, ax, **kwargs):
         assert len(kwargs['xy']) == 2, 'xy coord length is not equal to 2.'
         xy = kwargs['xy']
     ax.annotate(s=s, xy=xy, xycoords='figure fraction', fontsize=fs, weight='bold')
-
-
-
-
 
 
