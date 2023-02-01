d6754220176e0a61c960b0438be0f9979c038b6c
prajayshah <prajay.shah@mail.utoronto.ca>
2023-02-01 11:08:34 -0500

    changed name of package

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-import setuptools
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="reproducible_figures",
+setup(
+    name="repfigvis",
     version="0.1",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='http://github.com/prajayshah/reproducible_figures',
-    author="Prajay Shah",
-    author_email="prajay.shah@mail.utoronto.ca",
+    author="",
+    author_email="",
     description="Functions to help implement a reproducible figure making process in Python",
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
