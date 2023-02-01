bd1d002b569e4970576e263558fb1225ccb16a00
prajayshah <prajay.shah@mail.utoronto.ca>
2023-02-01 11:39:27 -0500

    changed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="repfigvis",
+    name="reproducible_figures",
     version="0.1",
     long_description=long_description,
     long_description_content_type='text/markdown',
